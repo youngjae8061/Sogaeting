aa72193d5357af6713fe445bdfbbbad4072d74ee
youngjae8061 <young3966@gmail.com>
2022-06-01 16:02:03 +0900

    회원가입 꾸미기

--- app/src/main/java/com/example/sogaeting/IntroActivity.kt
+++ app/src/main/java/com/example/sogaeting/IntroActivity.kt
@@ -1,14 +1,23 @@
 package com.example.sogaeting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sogaeting.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    lateinit var bind : ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        bind = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(bind.root)
 
 
+        bind.joinBtn.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/sogaeting/JoinActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sogaeting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+
+
+    }
+}
