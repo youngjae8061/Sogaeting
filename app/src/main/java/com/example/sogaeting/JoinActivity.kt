3a4507412fe2d1f5c95bd9b8c4901717a77152f3
youngjae8061 <young3966@gmail.com>
2022-06-01 16:17:35 +0900

    email, pwd 받아오기

--- app/src/main/java/com/example/sogaeting/JoinActivity.kt
+++ app/src/main/java/com/example/sogaeting/JoinActivity.kt
@@ -2,12 +2,22 @@ package com.example.sogaeting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sogaeting.databinding.ActivityJoinBinding
+import org.jetbrains.anko.toast
 
 class JoinActivity : AppCompatActivity() {
+    lateinit var bind : ActivityJoinBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+        bind = ActivityJoinBinding.inflate(layoutInflater)
+        setContentView(bind.root)
 
+        bind.join.setOnClickListener {
+
+            if(!bind.email.text.toString().isNullOrEmpty()) toast(" ${bind.email.text.toString()}")
+            if(!bind.password.text.toString().isNullOrEmpty()) toast(" ${bind.password.text.toString()}")
+        }
 
     }
 }
