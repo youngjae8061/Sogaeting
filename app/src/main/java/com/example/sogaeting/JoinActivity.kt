c6cea0b6cae609cf6bd223ecd2804c6bf1688635
youngjae8061 <young3966@gmail.com>
2022-06-01 16:41:25 +0900

    회원가입 구현

--- app/src/main/java/com/example/sogaeting/JoinActivity.kt
+++ app/src/main/java/com/example/sogaeting/JoinActivity.kt
@@ -1,23 +1,41 @@
 package com.example.sogaeting
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.sogaeting.databinding.ActivityJoinBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import org.jetbrains.anko.toast
 
 class JoinActivity : AppCompatActivity() {
     lateinit var bind : ActivityJoinBinding
+
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivityJoinBinding.inflate(layoutInflater)
         setContentView(bind.root)
 
+        auth = Firebase.auth
+
         bind.join.setOnClickListener {
-
-            if(!bind.email.text.toString().isNullOrEmpty()) toast(" ${bind.email.text.toString()}")
-            if(!bind.password.text.toString().isNullOrEmpty()) toast(" ${bind.password.text.toString()}")
+            auth.createUserWithEmailAndPassword(bind.email.text.toString(), bind.password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    }
+                }
         }
-
     }
 }
