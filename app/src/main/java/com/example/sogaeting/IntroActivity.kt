aede47ad8db043229e8327cc543ebe36e2fd8355
youngjae8061 <young3966@gmail.com>
2022-06-01 15:09:14 +0900

    Splash, Intro UI 꾸미기

---
+++ app/src/main/java/com/example/sogaeting/IntroActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sogaeting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/sogaeting/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.sogaeting
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(1000)
+            val intent = Intent(applicationContext, IntroActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
